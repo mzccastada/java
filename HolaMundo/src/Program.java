2b5506b0789a5b5d65e6fd4842203f4d3d7a833c
Castada Collado <mzcastada@indra.es>
2023-05-04 12:56:17 +0200

    .gitignore is now working

--- HolaMundo/src/Program.java
+++ HolaMundo/src/Program.java
@@ -1,9 +1,19 @@
 
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
 		System.out.println("Hola Mundo");
+		
+		//Tipos De Datos Primitivos
+		int numero = 20;
+		System.out.println(numero);
+		
+		double medio = 0.5;
+		System.out.println(medio);
+		
+		boolean verdadero = true;
+		System.out.println(verdadero);
+		
 	}
 
 }
